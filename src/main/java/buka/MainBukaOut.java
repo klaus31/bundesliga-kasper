47ca505d733ce8b05024e7498dea73c7ded0d345
klaus31 <danieloltmanns@gmx.de>
2015-04-07 14:46:39 +0200

    spieltag drop down, aber funktioniert nicht gut

--- src/main/java/buka/MainBukaOut.java
+++ src/main/java/buka/MainBukaOut.java
@@ -4,13 +4,18 @@ import java.util.List;
 
 import javafx.application.Application;
 import javafx.beans.binding.DoubleExpression;
+import javafx.beans.value.ChangeListener;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import buka.modelLibsAndDips.Partie;
@@ -26,24 +31,56 @@ public class MainBukaOut extends Application {
 
   private final double defaultHeight = Screen.getPrimary().getVisualBounds().getHeight();
   private final double defaultWidth = Screen.getPrimary().getVisualBounds().getWidth();
+  private Spieltag spieltag;
+  private Stage stage;
   private final TableView<BukaRow> table = new TableView<>();
 
-  private TableColumn getTableColumn(final String label, final String key, final double widthPercent) {
-    TableColumn partieCol = new TableColumn(label);
-    if (key != null) {
-      partieCol.setCellValueFactory(new PropertyValueFactory<>(key));
+  private Node getSpieltagComboBox() {
+    ObservableList<String> options = FXCollections.observableArrayList();
+    int i = 1;
+    while (i < 35) {
+      options.add("Spieltag " + i++);
     }
+    final ComboBox comboBox = new ComboBox(options);
+    comboBox.setValue("Spieltag " + spieltag.getNumber());
+    comboBox.valueProperty().addListener((ChangeListener<String>) (ov, oldSt, newSt) -> {
+      int newSpieltagNumber = Integer.parseInt(newSt.split(" ")[1]);
+      stage.hide();
+      spieltag = new SpieltagOpenLigaDB(newSpieltagNumber);
+      updateView();
+      stage.show();
+    });
+    return comboBox;
+  }
+
+  private TableColumn getTableColumn(final String label, final double widthPercent) {
+    TableColumn result = new TableColumn(label);
     final DoubleExpression width = table.widthProperty().multiply(widthPercent / 100);
-    partieCol.minWidthProperty().bind(width);
-    partieCol.prefWidthProperty().bind(width);
-    partieCol.maxWidthProperty().bind(width);
-    return partieCol;
+    result.minWidthProperty().bind(width);
+    result.prefWidthProperty().bind(width);
+    result.maxWidthProperty().bind(width);
+    return result;
+  }
+
+  private TableColumn getTableColumn(final String label, final String key, final double widthPercent) {
+    TableColumn result = getTableColumn(label, widthPercent);
+    result.setCellValueFactory(new PropertyValueFactory<>(key));
+    return result;
   }
 
   @Override
   public void start(final Stage stage) {
-    // model
-    final Spieltag spieltag = new SpieltagOpenLigaDB();
+    this.stage = stage;
+    spieltag = new SpieltagOpenLigaDB();
+    stage.setWidth(defaultWidth);
+    stage.setHeight(defaultHeight);
+    table.setEditable(false);
+    table.setMinWidth(defaultWidth);
+    stage.show();
+    updateView();
+  }
+
+  private void updateView() {
     final List<Partie> partien = spieltag.getPartien();
     final ObservableList<BukaRow> data = FXCollections.observableArrayList();
     partien.forEach(partie -> {
@@ -52,11 +89,6 @@ public class MainBukaOut extends Application {
     // javafx
     Scene scene = new Scene(new Group());
     stage.setTitle("Bundesliga Kasper - Spieltag " + spieltag.getNumber());
-    stage.setWidth(defaultWidth);
-    stage.setHeight(defaultHeight);
-    table.setEditable(false);
-    table.setMinWidth(defaultWidth);
-    table.setMinHeight(defaultHeight);
     final TableColumn colAnpfiff = getTableColumn("Anpfiff", "anpfiff", 9);
     final TableColumn colPartie = new TableColumn("Partie");
     colPartie.getColumns().add(getTableColumn("H", "partieHeim", 15));
@@ -73,9 +105,16 @@ public class MainBukaOut extends Application {
     colQuote.getColumns().add(getTableColumn("A", "quoteSiegAusw", 5));
     table.setItems(data);
     table.getColumns().addAll(colAnpfiff, colPartie, colTippAverage, colTipp1st, colQuote);
+    // layout
+    BorderPane border = new BorderPane();
+    HBox box1 = new HBox();
+    box1.getChildren().add(getSpieltagComboBox());
+    HBox box2 = new HBox();
+    box2.getChildren().add(table);
+    border.setTop(box1);
+    border.setBottom(box2);
     Group group = ((Group) scene.getRoot());
-    group.getChildren().addAll(table);
+    group.getChildren().add(border);
     stage.setScene(scene);
-    stage.show();
   }
 }
